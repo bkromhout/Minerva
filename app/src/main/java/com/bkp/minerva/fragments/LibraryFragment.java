c75f1a10764472fbb7980475f1f352505a038b42
Brenden Kromhout <bkromhout@gmail.com>
2016-01-13 11:10:58 -0500

    Ew comments.

--- app/src/main/java/com/bkp/minerva/fragments/LibraryFragment.java
+++ app/src/main/java/com/bkp/minerva/fragments/LibraryFragment.java
@@ -50,11 +50,6 @@ public class LibraryFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of this fragment using the provided parameters.
-     * @return A new instance of {@link LibraryFragment}.
-     */
-    // TODO: Rename and change types and number of parameters
     public static LibraryFragment newInstance() {
         LibraryFragment fragment = new LibraryFragment();
         Bundle args = new Bundle();
