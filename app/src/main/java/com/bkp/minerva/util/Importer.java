004c177d9f3a3d08c80940667ab55747b18a5a98
Brenden Kromhout <bkromhout@gmail.com>
2016-01-25 11:58:19 -0500

    Working on import flow
    Still fleshing stuff out.

--- app/src/main/java/com/bkp/minerva/util/Importer.java
+++ app/src/main/java/com/bkp/minerva/util/Importer.java
@@ -1,9 +1,13 @@
 package com.bkp.minerva.util;
 
+import android.util.Log;
 import com.bkp.minerva.C;
 import com.bkp.minerva.prefs.DefaultPrefs;
+import com.bkp.minerva.rx.RxBookTransformer;
 import com.bkp.minerva.rx.RxFileWalker;
+import nl.siegmann.epublib.domain.Book;
 import rx.Observable;
+import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
@@ -15,10 +19,12 @@ import java.util.List;
  */
 public class Importer {
     /**
-     * Help track the state of the importer.
+     * Various state the importer can be in.
+     * <p>
+     * Ready, Preparing, Running, Cancelling, Finishing.
      */
     private enum State {
-        READY, PREP, RUNNING, CANCELLING
+        READY, PREP, RUNNING, CANCELLING, FINISHING
     }
 
     /**
@@ -42,6 +48,15 @@ public class Importer {
      * Total number of files in the current run.
      */
     private long numTotal;
+    /**
+     * Subscription to an Rx flow which uses {@link RxFileWalker} to recursively walk down from a start directory in
+     * order to find files with specific extensions.
+     */
+    private Subscription fileWalkerSubscription;
+    /**
+     * Subscription to an Rx flow which uses
+     */
+    private Subscription fileImporterSubscription;
 
     /**
      * Get an instance of {@link Importer}.
@@ -60,9 +75,26 @@ public class Importer {
     /**
      * Starts a full import run. The importer will import files from the configured library directory.
      * <p>
-     * Calling this when the importer is already running will do nothing.
+     * Calling this when the importer isn't in a ready state will do nothing.
      */
     public void doFullImport() {
+        // Don't do anything if we aren't in a ready state.
+        if (currState != State.READY) return;
+
+        // Kick off preparations; flow will continue from there.
+        doFullImportPrep();
+    }
+
+    /**
+     * Prepare for a full import.
+     * <p>
+     * Validate the current library directory, then try to recursively get all files with specific extensions within
+     * that directory.
+     * <p>
+     * At the end of this method we pass off control to an Rx flow which uses {@link RxFileWalker} to get a list of
+     * files; once that flow produces a list, it will call {@link #onGotFileList(List)}.
+     */
+    private void doFullImportPrep() {
         currState = State.PREP;
 
         // Get and check currently configured library directory.
@@ -74,14 +106,18 @@ public class Importer {
         }
 
         // Get a list of files in the directory (and its subdirectories) which have certain extensions.
-        List<File> files = Observable.create(new RxFileWalker(currDir, C.VALID_EXTS))
-                                     .toList()
-                                     .subscribeOn(Schedulers.io())
-                                     .observeOn(AndroidSchedulers.mainThread())
-                                     .toBlocking()
-                                     .single();
-
-        // TODO something with the list of files!
+        // This will call through to onGotFileList() once it has the results.
+        //List<File> files = Observable.create(new RxFileWalker(currDir, C.VALID_EXTS)) TODO remove?
+        fileWalkerSubscription = Observable
+                .create(new RxFileWalker(currDir, C.VALID_EXTS))
+                .toList()
+                .single()
+                // TODO Not sure about the order of these 3 calls here...
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .unsubscribeOn(AndroidSchedulers.mainThread())
+                .doOnUnsubscribe(this::onFileWalkerUnsubscribe)
+                .subscribe(this::onGotFileList);
     }
 
     /**
@@ -96,14 +132,122 @@ public class Importer {
     }
 
     /**
+     * What to do when we unsubscribe from {@link #fileWalkerSubscription}.
+     */
+    private void onFileWalkerUnsubscribe() {
+        // Just null the reference to it. TODO is this enough??
+        fileWalkerSubscription = null;
+    }
+
+    /**
+     * Called by {@link #fileWalkerSubscription} when it calls {@code onNext()}.
+     * <p>
+     * This is part of full import preparation.
+     * @param files List of files with specific extensions found by {@link RxFileWalker}. These are the files we will
+     *              try to import.
+     */
+    private void onGotFileList(List<File> files) {
+        // Remove reference to subscription.
+        fileWalkerSubscription.unsubscribe();
+
+        // Check file list.
+        if (files.isEmpty()) {
+            // We don't have any files.
+            resetState();
+            return;
+        }
+
+        // Update state.
+        numDone = 0;
+        numTotal = files.size();
+
+        // Start actual importing.
+        doImportFiles(files);
+    }
+
+    /**
+     * Take the list of files we want to import and actually import them.
+     * <p>
+     * This is the point where we transition from preparing to running. At the end of this method we pass off control to
+     * an Rx flow. It will either call {@link #onAllFilesImported()} or {@link #onFileImporterError(Throwable)} when it
+     * is finished.
+     * @param files List of files to import.
+     */
+    private void doImportFiles(List<File> files) {
+        // Change state to running.
+        currState = State.RUNNING;
+
+        // Do importer flow.
+        fileImporterSubscription = Observable
+                .from(files)
+                .compose(new RxBookTransformer())
+                .subscribeOn(Schedulers.io()) // Everything above runs on the io thread pool.
+                .observeOn(AndroidSchedulers.mainThread()) // Switch to the main thread now!
+                .unsubscribeOn(AndroidSchedulers.mainThread())
+                .doOnNext(this::onGotBookFromFile) // TODO might want to make this better for Realm somehow.
+                .doOnUnsubscribe(this::onFileImporterUnsubscribe)
+                .subscribe(this::onImportedBookFile, this::onFileImporterError, this::onAllFilesImported);
+    }
+
+    /**
+     * What to do with a Book object we just got from a File.
+     * @param book Newly-obtained Book object.
+     */
+    private void onGotBookFromFile(Book book) {
+        // TODO!!
+    }
+
+    /**
+     * What to do when we've finished importing and processing a book file.
+     * <p>
+     * This is called at the end of the import flow, it should be lightweight!!
+     * @param book The Book we created from the file.
+     */
+    private void onImportedBookFile(Book book) {
+        // TODO!!
+    }
+
+    /**
+     * What to do if an error is thrown during import.
+     * @param t Throwable.
+     */
+    private void onFileImporterError(Throwable t) {
+        Log.e("Importer", "Error while importing and converting files.", t);
+        cancelFullImport();
+    }
+
+    /**
+     * What to do after we've finished importing all books.
+     */
+    private void onAllFilesImported() {
+        // We've finished importing all books.
+        // TODO
+    }
+
+    /**
+     * What to do when we unsubscribe from {@link #fileImporterSubscription}.
+     */
+    private void onFileImporterUnsubscribe() {
+        // Just null the reference to it. TODO is this enough??
+        fileImporterSubscription = null;
+    }
+
+    /**
      * Cancels the currently running full import.
      * <p>
-     * If the importer isn't running, this does nothing.
+     * Note that, at this time, calling this method will not roll back any changes which have already been made to the
+     * database.
+     * <p>
+     * If the importer isn't preparing or running, this does nothing.
      */
     public void cancelFullImport() {
-        if (currState == State.READY || currState == State.CANCELLING) return;
+        if (currState == State.READY || currState == State.CANCELLING || currState == State.FINISHING) return;
+
+        resetState();
 
         // TODO!!
+
+        // TODO make sure already applied changes are rolled back somehow??
     }
 
     /**
@@ -112,16 +256,24 @@ public class Importer {
      * BE CAREFUL!
      */
     private void resetState() {
-        currState = State.CANCELLING;
+        currState = State.FINISHING;
+
+        // Unsubscribe from the file walker subscription.
+        if (fileWalkerSubscription != null) fileWalkerSubscription.unsubscribe();
+        // Unsubscribe from the file walker subscription.
+        if (fileImporterSubscription != null) fileImporterSubscription.unsubscribe();
+
         this.currDir = null;
         this.numDone = -1;
         this.numTotal = -1;
+
         this.currState = State.READY;
     }
 
     /**
      * Check whether the importer is currently importing from some folder.
-     * @return True if the importer is currently preparing or running, false if the importer isn't running.
+     * @return True if the importer is currently not in the ready state, false otherwise.
+     * @see State
      */
     public boolean isRunning() {
         return currState != State.READY;
@@ -131,7 +283,7 @@ public class Importer {
      * The path to the directory that the importer is currently importing from.
      * @return Current import path, or null if the importer either isn't currently running, or is still preparing.
      */
-    public String getCurrDir() {
+    public String getCurrDirPath() {
         return currDir == null ? null : currDir.getAbsolutePath();
     }
 
