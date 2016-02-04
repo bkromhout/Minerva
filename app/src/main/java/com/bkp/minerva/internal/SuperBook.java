22e1db8e5796c0496a9fe5b48157c39809c3a1ad
Brenden Kromhout <bkromhout@gmail.com>
2016-02-03 23:58:15 -0500

    Lots of importer and Realm changes
    * Renamed Importer --> FullImporter
    * 95% of the FullImporter code is in place, just need the little UI-related things which communicate to the user what the progress is, ability to cancel (or not), rotation support, etc.
    * Fleshed out RBook
    * Added other R* classes (Realm objects)

---
+++ app/src/main/java/com/bkp/minerva/internal/SuperBook.java
@@ -0,0 +1,50 @@
+package com.bkp.minerva.internal;
+
+import com.bkp.minerva.util.BookUtils;
+import nl.siegmann.epublib.domain.Book;
+
+/**
+ * Holds a {@link nl.siegmann.epublib.domain.Book}, plus other data.
+ */
+public class SuperBook {
+
+    private final Book book;
+    private final String path;
+    private final String hash;
+
+    /**
+     * Create a new {@link SuperBook}.
+     * @param book The {@link Book} to hold.
+     * @param path The path (relative to the library dir) to the book file.
+     * @param hash The hash of the book file.
+     */
+    public SuperBook(Book book, String path, String hash) {
+        if (book == null || book.getTitle() == null || book.getTitle().isEmpty() || BookUtils.getFirstAuthor(book) == null
+                || path == null || path.isEmpty()) throw new IllegalArgumentException();
+
+        this.book = book;
+        this.path = path;
+        this.hash = hash;
+    }
+
+    /**
+     * Create a new {@link SuperBook}.
+     * @param book The {@link Book} to hold.
+     * @param path The path (relative to the library dir) to the book file.
+     */
+    public SuperBook(Book book, String path) {
+        this(book, path, null);
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+}
