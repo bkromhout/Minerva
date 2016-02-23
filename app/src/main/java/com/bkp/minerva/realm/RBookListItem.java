a6d8b379505399945ce4184f8e1e86335103be2b
Brenden Kromhout <bkromhout@gmail.com>
2016-02-22 21:52:49 -0500

    Need a transaction when we modify things.

--- app/src/main/java/com/bkp/minerva/realm/RBookListItem.java
+++ app/src/main/java/com/bkp/minerva/realm/RBookListItem.java
@@ -1,6 +1,7 @@
 package com.bkp.minerva.realm;
 
 import com.bkp.minerva.C;
+import io.realm.Realm;
 import io.realm.RealmObject;
 import io.realm.annotations.Index;
 import io.realm.annotations.PrimaryKey;
@@ -62,7 +63,9 @@ public class RBookListItem extends RealmObject {
 
         // Position is the next position number from owningList. Then we update the next position number.
         this.pos = owningList.getNextPos();
-        owningList.setNextPos(this.pos + C.LIST_ITEM_GAP);
+        try (Realm realm = Realm.getDefaultInstance()) {
+            realm.executeTransaction(tRealm -> owningList.setNextPos(this.pos + C.LIST_ITEM_GAP));
+        }
     }
 
     /**
