e833d840e65f7e38ecfef1f85e9ce90c9f80d570
Brenden Kromhout <bkromhout@gmail.com>
2016-04-10 17:44:19 -0400

    Finished layout for BookInfoActivity, working on code

--- app/src/main/java/com/bkromhout/minerva/BookInfoActivity.java
+++ app/src/main/java/com/bkromhout/minerva/BookInfoActivity.java
@@ -1,19 +1,32 @@
 package com.bkromhout.minerva;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.bkromhout.minerva.events.ActionEvent;
+import com.bkromhout.minerva.prefs.DefaultPrefs;
 import com.bkromhout.minerva.realm.RBook;
+import com.bkromhout.minerva.util.Dialogs;
 import com.bkromhout.minerva.util.Util;
+import com.greenfrvr.hashtagview.HashtagView;
 import io.realm.Realm;
+import org.greenrobot.eventbus.Subscribe;
 
 /**
  * Displays information for some {@link com.bkromhout.minerva.realm.RBook}.
@@ -22,7 +35,15 @@ public class BookInfoActivity extends AppCompatActivity {
     // Key strings for the bundle passed when this activity is started.
     public static final String BOOK_SEL_STR = "BOOK_SEL_STR";
 
-    // Views.
+    /**
+     * Part of the information for which views are conditionally shown.
+     * @see #togglePart(Part, boolean)
+     */
+    private enum Part {
+        TAGS, LISTS, SUBJECTS, TYPES, FORMAT, LANGUAGE, PUBLISHER, PUBLISH_DATE, MOD_DATE
+    }
+
+    // AppBarLayout views.
     @Bind(R.id.toolbar)
     Toolbar toolbar;
     @Bind(R.id.collapsing_toolbar)
@@ -32,6 +53,65 @@ public class BookInfoActivity extends AppCompatActivity {
     @Bind(R.id.fab)
     FloatingActionButton fab;
 
+    // Views which always are shown.
+    @Bind(R.id.title)
+    TextView title;
+    @Bind(R.id.author)
+    TextView author;
+    @Bind(R.id.desc)
+    TextView desc;
+    @Bind(R.id.chap_count)
+    TextView chapCount;
+    @Bind(R.id.rating)
+    RatingBar rating;
+    @Bind(R.id.edit_rating)
+    ImageButton editRating;
+    @Bind(R.id.edit_tags)
+    ImageButton editTags;
+    @Bind(R.id.path)
+    TextView path;
+    @Bind(R.id.last_read_date)
+    TextView lastReadDate;
+    @Bind(R.id.last_import_date)
+    TextView lastImportDate;
+
+    // Views which are conditionally shown.
+    @Bind(R.id.tags)
+    HashtagView tags;
+    @Bind(R.id.no_tags)
+    TextView noTags;
+    @Bind(R.id.lists)
+    TextView lists;
+    @Bind(R.id.no_lists)
+    TextView noLists;
+    @Bind(R.id.lbl_subjects)
+    TextView lblSubjects;
+    @Bind(R.id.subjects)
+    TextView subjects;
+    @Bind(R.id.lbl_types)
+    TextView lblTypes;
+    @Bind(R.id.types)
+    TextView types;
+    @Bind(R.id.lbl_format)
+    TextView lblFormat;
+    @Bind(R.id.format)
+    TextView format;
+    @Bind(R.id.lbl_language)
+    TextView lblLanguage;
+    @Bind(R.id.language)
+    TextView language;
+    @Bind(R.id.lbl_publisher)
+    TextView lblPublisher;
+    @Bind(R.id.publisher)
+    TextView publisher;
+    @Bind(R.id.lbl_publish_date)
+    TextView lblPublishDate;
+    @Bind(R.id.publish_date)
+    TextView publishDate;
+    @Bind(R.id.lbl_mod_date)
+    TextView lblModDate;
+    @Bind(R.id.mod_date)
+    TextView modDate;
 
     /**
      * Relative path to use to find an {@link com.bkromhout.minerva.realm.RBook}.
@@ -81,18 +161,6 @@ public class BookInfoActivity extends AppCompatActivity {
         relPath = b.getString(BOOK_SEL_STR, null);
     }
 
-    /**
-     * Init the UI.
-     */
-    private void initUi() {
-        // Set title.
-        collapsibleToolbar.setTitle(book.getTitle());
-
-        // TODO Set cover image.
-
-        // TODO
-    }
-
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
         Util.forceMenuIcons(menu, this, getClass().getSimpleName());
@@ -122,6 +190,8 @@ public class BookInfoActivity extends AppCompatActivity {
             case R.id.action_read:
                 // Open the book file.
                 book.openFileUsingIntent(this);
+                // TODO Refresh last read date view.
+
                 return true;
             case android.R.id.home:
                 onBackPressed();
@@ -132,11 +202,147 @@ public class BookInfoActivity extends AppCompatActivity {
     }
 
     /**
+     * Called when we wish to take some action.
+     * @param event {@link ActionEvent}.
+     */
+    @Subscribe
+    public void onActionEvent(ActionEvent event) {
+        switch (event.getActionId()) {
+            case R.id.action_rate: {
+                // Save rating and update UI.
+                realm.executeTransaction(tRealm -> book.setRating((Integer) event.getData()));
+                rating.setNumStars((Integer) event.getData());
+                break;
+            }
+            case R.id.action_add_to_list: {
+                //RBookList list = realm.where(RBookList.class).equalTo("name", (String) event.getData()).findFirst();
+                //RBookList.addBooks(list, selectedItems);
+                break;
+            }
+            /*case R.id.action_re_import: {
+                ReImporter.reImportBooks(selectedItems, this);
+                return;
+            }*/
+            /*case R.id.action_delete: {
+                // Delete the RBooks from Realm.
+                List<String> relPaths = RBook.deleteBooks(selectedItems);
+                // If the user wants us to, also try to delete the corresponding files from the device.
+                if ((boolean) event.getData()) {
+                    for (String relPath : relPaths) {
+                        File file = Util.getFileFromRelPath(relPath);
+                        if (file != null) file.delete();
+                    }
+                }
+                break;
+            }*/
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case C.RC_TAG_ACTIVITY: {
+                // Came back from TaggingActivity.
+                if (resultCode == Activity.RESULT_OK) {
+                    // We've changed the tags on some books.
+                    // TODO Check book's tags, then toggle tags view based on that.
+                }
+                break;
+            }
+        }
+    }
+
+    /**
      * Open the book when the FAB is clicked.
      */
     @OnClick(R.id.fab)
     void onReadFabClicked() {
         // Open the book file.
         book.openFileUsingIntent(this);
+    }
+
+    /**
+     * Open the rating dialog when the edit rating button is clicked.
+     */
+    @OnClick(R.id.edit_rating)
+    void onEditRatingClicked() {
+        // Open the rating dialog.
+        Dialogs.ratingDialog(this, book.getRating());
+    }
+
+    /**
+     * Open the tagging dialog when the edit tags button is clicked.
+     */
+    @OnClick(R.id.edit_tags)
+    void onEditTagsClicked() {
+        // Open the tagging activity.
+        TaggingActivity.start(this, book);
+    }
+
+    /**
+     * Init the UI.
+     */
+    @SuppressLint("SetTextI18n")
+    private void initUi() {
+        // Set title.
+        collapsibleToolbar.setTitle(book.getTitle());
+
+        // TODO Set cover image.
+        coverImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.epub_logo_color));
+
+        // Fill in common views using book data.
+        title.setText(book.getTitle());
+        author.setText(book.getAuthor());
+        desc.setText(book.getDesc());
+        chapCount.setText(book.getNumChaps());
+        rating.setNumStars(book.getRating());
+        path.setText(DefaultPrefs.get().getLibDir("") + book.getRelPath());
+
+    }
+
+    /**
+     * Show/hide the given {@code part}.
+     * @param part Part to show/hide.
+     * @param show If true, show the part, otherwise hide it.
+     */
+    private void togglePart(Part part, boolean show) {
+        switch (part) {
+            case TAGS:
+                tags.setVisibility(show ? View.VISIBLE : View.GONE);
+                noTags.setVisibility(show ? View.GONE : View.VISIBLE);
+                break;
+            case LISTS:
+                lists.setVisibility(show ? View.VISIBLE : View.GONE);
+                noLists.setVisibility(show ? View.GONE : View.VISIBLE);
+                break;
+            case SUBJECTS:
+                lblSubjects.setVisibility(show ? View.VISIBLE : View.GONE);
+                subjects.setVisibility(show ? View.VISIBLE : View.GONE);
+                break;
+            case TYPES:
+                lblTypes.setVisibility(show ? View.VISIBLE : View.GONE);
+                types.setVisibility(show ? View.VISIBLE : View.GONE);
+                break;
+            case FORMAT:
+                lblFormat.setVisibility(show ? View.VISIBLE : View.GONE);
+                format.setVisibility(show ? View.VISIBLE : View.GONE);
+                break;
+            case LANGUAGE:
+                lblLanguage.setVisibility(show ? View.VISIBLE : View.GONE);
+                language.setVisibility(show ? View.VISIBLE : View.GONE);
+                break;
+            case PUBLISHER:
+                lblPublisher.setVisibility(show ? View.VISIBLE : View.GONE);
+                publisher.setVisibility(show ? View.VISIBLE : View.GONE);
+                break;
+            case PUBLISH_DATE:
+                lblPublishDate.setVisibility(show ? View.VISIBLE : View.GONE);
+                publishDate.setVisibility(show ? View.VISIBLE : View.GONE);
+                break;
+            case MOD_DATE:
+                lblModDate.setVisibility(show ? View.VISIBLE : View.GONE);
+                modDate.setVisibility(show ? View.VISIBLE : View.GONE);
+                break;
+        }
     }
 }
