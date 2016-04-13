964d466c9a140c3748e04e05f359aed947410350
Brenden Kromhout <bkromhout@gmail.com>
2016-04-12 22:17:21 -0400

    Create ActionHelper class

---
+++ app/src/main/java/com/bkromhout/minerva/data/ActionHelper.java
@@ -0,0 +1,8 @@
+package com.bkromhout.minerva.data;
+
+/**
+ * Convenience class which provides static methods to execute data-related actions, preventing the need for duplicate
+ * code.
+ */
+public class ActionHelper {
+}
