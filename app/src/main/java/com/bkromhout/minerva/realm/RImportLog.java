07fd6d0bb3203a85cccac9b11fa3f4d670798b04
Brenden Kromhout <bkromhout@gmail.com>
2016-05-09 15:46:42 -0400

    Create RImportLog object to help us save import logs to Realm

---
+++ app/src/main/java/com/bkromhout/minerva/realm/RImportLog.java
@@ -0,0 +1,64 @@
+package com.bkromhout.minerva.realm;
+
+import io.realm.RealmObject;
+
+/**
+ * Represents an import log in Realm.
+ */
+public class RImportLog extends RealmObject {
+    /**
+     * Time when the import ended in milliseconds.
+     */
+    public long endTime;
+    /**
+     * Full import log.
+     */
+    public String fullLog;
+    /**
+     * Errors-only import log.
+     */
+    public String errorLog;
+    /**
+     * Whether or not the import was a success.
+     */
+    public boolean wasSuccess;
+
+    /**
+     * Create a default {@link RImportLog}.
+     */
+    public RImportLog() {
+        endTime = -1;
+        fullLog = null;
+        errorLog = null;
+        wasSuccess = false;
+    }
+
+    /**
+     * Create a new {@link RImportLog}.
+     * @param endTime    Time when the import ended, in milliseconds.
+     * @param fullLog    Full import log.
+     * @param errorLog   Errors-only import log.
+     * @param wasSuccess Whether or not the import was a success.
+     */
+    public RImportLog(long endTime, String fullLog, String errorLog, boolean wasSuccess) {
+        this.endTime = endTime;
+        this.fullLog = fullLog;
+        this.errorLog = errorLog;
+        this.wasSuccess = wasSuccess;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RImportLog that = (RImportLog) o;
+
+        return endTime == that.endTime;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (endTime ^ (endTime >>> 32));
+    }
+}
