6d0367b28dba7f3722f78302b6f6ad3442b9da26
Brenden Kromhout <bkromhout@gmail.com>
2016-07-09 21:10:22 -0400

    Rework WelcomeActivity to use material-intro lib

--- app/src/main/java/com/bkromhout/minerva/activities/WelcomeActivity.java
+++ app/src/main/java/com/bkromhout/minerva/activities/WelcomeActivity.java
@@ -1,32 +1,47 @@
 package com.bkromhout.minerva.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.afollestad.materialdialogs.folderselector.FolderChooserDialog;
 import com.bkromhout.minerva.Minerva;
 import com.bkromhout.minerva.R;
 import com.bkromhout.minerva.data.BackupUtils;
+import com.bkromhout.minerva.data.DataUtils;
 import com.bkromhout.minerva.data.Importer;
 import com.bkromhout.minerva.events.PermGrantedEvent;
 import com.bkromhout.minerva.ui.SnackKiosk;
 import com.bkromhout.minerva.util.Util;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.app.NavigationPolicy;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+import com.heinrichreimersoftware.materialintro.slide.Slide;
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import rx.Observable;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class WelcomeActivity extends PermCheckingActivity implements SnackKiosk.Snacker,
-        FolderChooserDialog.FolderCallback {
+/**
+ * Fancy welcome activity.
+ */
+public class WelcomeActivity extends IntroActivity implements SnackKiosk.Snacker, FolderChooserDialog.FolderCallback {
+    private static final int WELCOME = 0;
+    private static final int PERMISSIONS = 1;
+    private static final int CHOOSE_FOLDER = 2;
+    private static final int RESTORE_DB = 3;
+    private static final int FINISHED = 4;
+
     /**
      * Due to the way that our permissions handling works, we have to make sure that we wait to retry things until
      * onResume is called. These are the possible options currently.
@@ -35,20 +50,6 @@ public class WelcomeActivity extends PermCheckingActivity implements SnackKiosk.
         OPEN_F_CHOOSER, SHOW_DB_BACKUPS_LIST, START_IMPORT
     }
 
-    // Views
-    @BindView(R.id.base)
-    ViewGroup base;
-    @BindView(R.id.lbl_folder)
-    TextView tvFolderLbl;
-    @BindView(R.id.folder)
-    TextView tvFolder;
-    @BindView(R.id.first_import_prompt)
-    TextView tvFirstImportPrompt;
-    @BindView(R.id.start_full_import)
-    Button btnStartFullImport;
-    @BindView(R.id.restore_db_backup)
-    Button btnRestoreDb;
-
     /**
      * What deferred action to take when {@link #onResume()} is called.
      */
@@ -56,13 +57,35 @@ public class WelcomeActivity extends PermCheckingActivity implements SnackKiosk.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setFullscreen(true);
         super.onCreate(savedInstanceState);
-        setResult(RESULT_CANCELED);
 
-        setContentView(R.layout.activity_welcome);
-        ButterKnife.bind(this);
+        setButtonBackFunction(BUTTON_BACK_FUNCTION_BACK);
 
-        initAndContinuePermChecksIfNeeded();
+        setNavigationPolicy(new NavigationPolicy() {
+            @Override
+            public boolean canGoForward(int whichScreen) {
+                switch (whichScreen) {
+                    case PERMISSIONS:
+                        return ContextCompat.checkSelfPermission(WelcomeActivity.this,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+                    case CHOOSE_FOLDER:
+                        return Util.tryResolveDir(Minerva.prefs().getLibDir(null)) != null;
+                    case WELCOME:
+                    case RESTORE_DB:
+                    case FINISHED:
+                    default:
+                        return true;
+                }
+            }
+
+            @Override
+            public boolean canGoBackward(int whichScreen) {
+                return whichScreen != WELCOME;
+            }
+        });
+
+        addSlides(makeSlides());
     }
 
     @Override
@@ -84,7 +107,7 @@ public class WelcomeActivity extends PermCheckingActivity implements SnackKiosk.
                     onChooseFolderClick();
                     break;
                 case SHOW_DB_BACKUPS_LIST:
-                    // TODO
+                    onRestoreDbClick();
                     break;
                 case START_IMPORT:
                     onStartFullImportClicked();
@@ -124,20 +147,69 @@ public class WelcomeActivity extends PermCheckingActivity implements SnackKiosk.
         }
     }
 
-    @Override
-    public void onFolderSelection(@NonNull FolderChooserDialog dialog, @NonNull File folder) {
-        String path = folder.getAbsolutePath();
-        Minerva.prefs().putLibDir(path);
+    private List<Slide> makeSlides() {
+        ArrayList<Slide> slides = new ArrayList<>();
 
-        // Set the path of the folder to the textview.
-        tvFolder.setText(path);
+        slides.add(new FragmentSlide.Builder()
+                .fragment(R.layout.welcome_logo_slide)
+                .background(R.color.brown500)
+                .backgroundDark(R.color.brown700)
+                .build());
 
-        // Show the rest of the views.
-        tvFolderLbl.setVisibility(View.VISIBLE);
-        tvFolder.setVisibility(View.VISIBLE);
-        tvFirstImportPrompt.setVisibility(View.VISIBLE);
-        btnStartFullImport.setVisibility(View.VISIBLE);
-        btnRestoreDb.setVisibility(View.VISIBLE);
+        slides.add(new SimpleSlide.Builder()
+                .title(R.string.storage_permission_needed)
+                .description(R.string.storage_permission_rationale)
+                .image(R.drawable.welcome_micro_sd_card)
+                .background(R.color.red500)
+                .backgroundDark(R.color.red700)
+                .build());
+
+        slides.add(new SimpleSlide.Builder()
+                .layout(R.layout.welcome_extra_text_slide)
+                .title(R.string.choose_library_dir)
+                .description(R.string.choose_library_dir_desc)
+                .image(R.drawable.welcome_book)
+                .background(R.color.orange500)
+                .backgroundDark(R.color.orange700)
+                .buttonCtaLabel(R.string.open_folder_chooser)
+                .buttonCtaClickListener(view -> onChooseFolderClick())
+                .build());
+
+        slides.add(new SimpleSlide.Builder()
+                .title(R.string.title_restore_db)
+                .description(R.string.restore_database_desc)
+                .image(R.drawable.welcome_restore_database)
+                .background(R.color.blueGrey500)
+                .backgroundDark(R.color.blueGrey700)
+                .buttonCtaLabel(R.string.choose_database_backup)
+                .buttonCtaClickListener(view -> onRestoreDbClick())
+                .build());
+
+        slides.add(new SimpleSlide.Builder()
+                .title(R.string.ready_to_go)
+                .description(R.string.ready_to_go_desc)
+                .image(R.drawable.welcome_ready_to_go)
+                .background(R.color.green500)
+                .backgroundDark(R.color.green700)
+                .build());
+
+        return slides;
+    }
+
+    /**
+     * Sets the text of the folder text view.
+     * @param folderPath Path to the chosen library folder.
+     */
+    private void setFolderText(String folderPath) {
+        View root = getSlide(CHOOSE_FOLDER).getFragment().getView();
+        TextView desc = root != null ? (TextView) root.findViewById(R.id.mi_description) : null;
+        TextView extra = root != null ? (TextView) root.findViewById(R.id.extra_slide_text) : null;
+        boolean notNullOrEmpty = folderPath != null && !folderPath.isEmpty();
+        if (extra != null && notNullOrEmpty) {
+            if (desc != null) extra.setTextColor(desc.getTextColors());
+            extra.setVisibility(View.VISIBLE);
+            extra.setText(DataUtils.toSpannedHtml(String.format(getString(R.string.lbl_folder), folderPath)));
+        } else if (extra != null) extra.setVisibility(View.GONE);
     }
 
     /**
@@ -162,8 +234,7 @@ public class WelcomeActivity extends PermCheckingActivity implements SnackKiosk.
     /**
      * Trigger a full import, then finish and show the {@link ImportActivity} when clicked.
      */
-    @OnClick(R.id.start_full_import)
-    void onStartFullImportClicked() {
+    private void onStartFullImportClicked() {
         if (!Util.checkForStoragePermAndFireEventIfNeeded(R.id.action_import)) return;
         Importer.get().queueFullImport();
         Minerva.prefs().setFirstImportTriggered();
@@ -174,8 +245,7 @@ public class WelcomeActivity extends PermCheckingActivity implements SnackKiosk.
     /**
      * Click handler for the restore DB item.
      */
-    @OnClick(R.id.restore_db_backup)
-    void onRestoreDbClick() {
+    private void onRestoreDbClick() {
         if (!Util.checkForStoragePermAndFireEventIfNeeded(R.id.action_restore_db)) return;
         // Get a list of backed up realm files. If there aren't any, tell the user that and we're done.
         final File[] backedUpRealmFiles = BackupUtils.getRestorableRealmFiles();
@@ -204,9 +274,16 @@ public class WelcomeActivity extends PermCheckingActivity implements SnackKiosk.
                 .show();
     }
 
+    @Override
+    public void onFolderSelection(@NonNull FolderChooserDialog dialog, @NonNull File folder) {
+        String path = folder.getAbsolutePath();
+        Minerva.prefs().putLibDir(path);
+        setFolderText(path);
+    }
+
     @NonNull
     @Override
     public View getSnackbarAnchorView() {
-        return base;
+        return findViewById(R.id.mi_frame);
     }
 }
